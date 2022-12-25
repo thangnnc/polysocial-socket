ac4857a8531957c7bd76deb2d07a873f2a12077c
thangnnc <77538291+thangnncps14579@users.noreply.github.com>
2022-12-26 05:45:27 +0700

    no message

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const io = new Server(server, {
   },
 });
 
-console.log(PORT);
-
-app.get('/port', (req, resp) => {
+app.get('/', (req, resp) => {
   resp.write(`<h2>Socket start on port: ${PORT}</h2>`);
   resp.end();
 })
@@ -58,14 +56,14 @@ io.on("connection", async function (socket) {
     socket.to(userId).emit("request_accept");
   });
 
-   socket.on("delete_friend", function (userId) {
-    socket.to(userId).emit("request_delete_friend",listSocket);
+  socket.on("delete_friend", function (userId) {
+    socket.to(userId).emit("request_delete_friend", listSocket);
     socket.to(userId).emit("request_delete_friend_profile");
-    socket.emit("request_delete_friend",listSocket);
+    socket.emit("request_delete_friend", listSocket);
   });
 
-  socket.on("accept_friend", function (userId) {  
-    socket.to(userId).emit("successful_accept",listSocket);
+  socket.on("accept_friend", function (userId) {
+    socket.to(userId).emit("successful_accept", listSocket);
     socket.emit("successful_accept", listSocket);
   });
 
@@ -77,55 +75,68 @@ io.on("connection", async function (socket) {
     socket.emit("reset_one_account_get_One_All_Notification");
   });
 
-    socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log("User joined room: " + data);
-    });
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("User joined room: " + data);
+  });
 
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("recevie_message", data.content);
-      socket.to(data.room).emit("recevie_message_name", listSocket);
-      socket.emit("recevie_message_name", listSocket);
+  socket.on("leave_room", (data) => {
+    socket.leave(data);
+    console.log("User leave room: " + data);
+  });
 
-    });
+  socket.on("send_message", (data) => {
+    socket.broadcast.to(data.room).emit("recevie_message", data.content);
+    socket.to(data.room).emit("recevie_message_name", listSocket);
+    socket.emit("recevie_message_name", listSocket);
+    // socket.emit("recevie_message_name", listSocket);
+  });
 
-    socket.on("isSeen", () => {
-      socket.emit("recevie_message_name", listSocket);
-    });
+  socket.on("isSeen", () => {
+    socket.emit("recevie_message_name", listSocket);
+  });
 
-    socket.on("add_member_group", function () {
-      io.sockets.emit("reset_member_group_successful",listSocket);
-    });
+  socket.on("add_member_group", function () {
+    io.sockets.emit("reset_member_group_successful", listSocket);
+  });
 
-    socket.on("create_group", function (userId) {
-      socket.to(userId).emit("reset_create_group_successful",listSocket);
-      // socket.emit("reset_create_group_successful",listSocket);
-    });
-    socket.on("delete_member_group", function (userId) {
-      io.sockets.emit("reset_delete_member",listSocket);
-      socket.to(userId).emit("reset_delete_member_notification",listSocket);
+  socket.on("create_group", function (userId) {
+    socket.to(userId).emit("reset_create_group_successful", listSocket);
+    // socket.emit("reset_create_group_successful",listSocket);
+  });
+  socket.on("delete_member_group", function (userId) {
+    io.sockets.emit("reset_delete_member", listSocket);
+    socket.to(userId).emit("reset_delete_member_notification", listSocket);
 
-      // socket.to(userId).emit("reset_create_group_successful",listSocket);
-      // socket.emit("reset_create_group_successful",listSocket);
-    });
+    // socket.to(userId).emit("reset_create_group_successful",listSocket);
+    // socket.emit("reset_create_group_successful",listSocket);
+  });
+
+  socket.on("Client_request_create_like_comment", function (userId) {
+    socket.broadcast.emit("Server_response_like_comment");
+    socket.to(userId).emit("reset_like_notification");
     
-    socket.on("Client_request_create_like_comment", function () {
-      socket.broadcast.emit("Server_response_like_comment");
-    });
+  });
+  //message
+  socket.on("I'm typing", (room, data) => {
+    // var rooms = socket.rooms;
 
-    socket.on("I'm typing", (room, data) => {
-      socket.broadcast.emit(
-        room + "user-typing",
-        data,
-        data.fullName + " đang soạn tin nhắn..."
-      );
-    });
+    socket
+      .to(room)
+      .emit("user-typing", data, data.fullName + " đang soạn tin nhắn...");
+  });
 
-    socket.on("I stopped typing", (room) => {
-      socket.broadcast.emit(room + "stop-user-typing", "");
-    });
+  socket.on("I stopped typing", (room) => {
+    socket.to(room).emit("stop-user-typing", "");
+  });
+  //comment
+  socket.on("I'm_typing_comment", (postId) => {
+    socket.broadcast.emit("user_typing_comment", "Có người đang bình luận...",postId);
+  });
 
-  
+  socket.on("I_stopped_typing_comment", (postId) => {
+    socket.broadcast.emit("stop_user_typing_comment", "",postId);
+  });
 
   socket.on("disconnect", function () {
     console.log(socket.id + " ngat ket noi!!!!!!");
